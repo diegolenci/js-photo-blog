cee7103c8097e20732e2abad1c52c405af5ecddd
diegolenci <lencidiego0@gmail.com>
2025-01-29 15:01:54 +0100

    creazione cards con immagine, testo e data

---
+++ main.js
@@ -0,0 +1,27 @@
+
+//ENDPOINT
+fetch("https://lanciweb.github.io/demo/api/pictures/")
+.then(response => response.json())
+.then(data => {
+
+    console.log(data) //ARRAY DELLE 6 CARDS
+
+    data.forEach(element => {
+        console.log(element)
+        document.querySelector('.container .row').innerHTML += 
+    //STRUTTURA DELLE CARDS
+    `
+    <div class="col-3">
+        <div class="card">
+            <img src="${element.url}">
+            <h4 id="card-title" class="card-title">${element.title}</h4>
+            <p id="card-text">${element.date}</p>
+        </div>
+    </div>
+    `
+    });
+
+})
+.catch(error => {
+    console.error(error)
+})
